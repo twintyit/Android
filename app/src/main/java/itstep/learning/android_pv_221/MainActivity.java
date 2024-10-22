a8ef60404bd9b1c145fcb2b3dcd435fe4eab71f3
Kamyshov Ruslan <kamyshov02u@gmail.com>
2024-10-22 21:14:17 +0200

    first commit

---
+++ app/src/main/java/itstep/learning/android_pv_221/MainActivity.java
@@ -0,0 +1,31 @@
+package itstep.learning.android_pv_221;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        findViewById( R.id.main_btn_calc ).setOnClickListener(this::onCalcButtonClick);
+    }
+
+    private void onCalcButtonClick(View view){
+        Toast.makeText(this, "There will be a calculator here", Toast.LENGTH_SHORT).show();
+    }
+}
