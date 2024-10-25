40cf77865738361a0b73b99586171d032168841b
Kamyshov Ruslan <kamyshov02u@gmail.com>
2024-10-25 21:21:22 +0200

    containers

--- app/src/main/java/itstep/learning/android_pv_221/CalcActivity.java
+++ app/src/main/java/itstep/learning/android_pv_221/CalcActivity.java
@@ -1,9 +1,6 @@
 package itstep.learning.android_pv_221;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,23 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class CalcActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_calc);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-        findViewById( R.id.main_btn_calc ).setOnClickListener(this::onCalcButtonClick);
-    }
-
-    private void onCalcButtonClick(View view){
-        Intent intent = new Intent(this, CalcActivity.class);
-        startActivity( intent );
     }
 }
